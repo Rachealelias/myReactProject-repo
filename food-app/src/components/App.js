94443acf4c0733a02f0b162e1485e4e456c622f4
Racheal Amuneke <rachealsamuel708@gmail.com>
2021-12-28 09:35:45 -0500

    created router-dom installation & navigations

--- food-app/src/components/App.js
+++ food-app/src/components/App.js
@@ -1,9 +1,43 @@
   import './App.css';
+  import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+  import FoodForm from './foods/FoodForm';
+  import FoodContainer from '../containers/FoodContainer';
+  import FoodCard from './foods/FoodCard';
+  import NavBar from './navigation/NavBar'; 
+  import Header from './navigation/Header';
+  import Footer from './navigation/Footer';
+  import Home from './Home';
+  import About from './About';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <NavBar />
+        <Header />
+        <Switch>
+          <Route path="/foods/new">
+            <FoodForm />
+          </Route>
+
+          <Route path="/foods/:id">
+            <FoodCard />
+          </Route>
+
+          <Route path="/foods">
+            <FoodContainer />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }

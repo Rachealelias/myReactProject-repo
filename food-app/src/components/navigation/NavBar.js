6301f202183e30b58da09d2549cc564f08e07e28
Racheal Amuneke <rachealsamuel708@gmail.com>
2021-12-29 13:40:44 -0500

    navlink created

--- food-app/src/components/navigation/NavBar.js
+++ food-app/src/components/navigation/NavBar.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const style = {
+    width: "60%",
+    margin: "5% 0 1%",
+    padding: "1em",
+    textDecoration:"none",
+    color:"blue",
+    backgroundColor: "rgb()",
+    fontWeight: "bold",
+    verticalAlign: "center"
+}
 
 function NavBar() {
     return (
         <div>
-            
+           <NavLink
+           activeStyle={{
+               fontWeight:"bolder",
+               color: "red"
+           }}
+           exactstyle={style}
+           to="/"
+           >Home</NavLink> 
+
+            <NavLink
+           activeStyle={{
+               fontWeight:"bolder",
+               color: "red"
+           }}
+           exactstyle={style}
+           to="/about"
+           >About</NavLink> 
+
+            <NavLink
+           activeStyle={{
+               fontWeight:"bolder",
+               color: "red"
+           }}
+           exactstyle={style}
+           to="/foods"
+           >Foods</NavLink> 
+
+            <NavLink
+           activeStyle={{
+               fontWeight:"bolder",
+               color: "red"
+           }}
+           exactstyle={style}
+           to="/foods/new"
+           >Add Food</NavLink> 
         </div>
     )
 }

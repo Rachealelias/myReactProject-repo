05ef14fb9984f25c2a441cbaef5db1037a8b6121
Racheal Amuneke <rachealsamuel708@gmail.com>
2021-12-30 21:01:20 -0500

    Footer created

--- food-app/src/components/navigation/Footer.js
+++ food-app/src/components/navigation/Footer.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 function Footer() {
+    const getYear = () => {
+        let currentYear = new Date().getFullYear();
+        return currentYear
+    }
     return (
-        <div className='footer'>
+        <div className='footer' >
+            <footer>
+                <div>
+                    <p>
+                        Copyright &copy;
+                        <span>{getYear()} Racheal Amuneke</span>
+                        <br/> Made with 😍 by Racheal 
+                    </p>
+                </div>
+            </footer>
             
         </div>
     )

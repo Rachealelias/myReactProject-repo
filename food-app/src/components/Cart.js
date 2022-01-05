d60e75d90cf0710c3d7050fcc385530c6edf1c5e
Racheal Amuneke <rachealsamuel708@gmail.com>
2022-01-05 15:27:09 -0500

    cart component in progress

--- food-app/src/components/Cart.js
+++ food-app/src/components/Cart.js
@@ -1,33 +1,28 @@
-// import React,{useState, useEffect} from 'react'
-// import FoodCard from './foods/FoodCard';
-// //import FoodDetails from './foods/FoodDetails';
+import React,{useState, useEffect} from 'react'
+import FoodCard from './foods/FoodCard';
 
 
-// function Cart() {
-//     const [cart, setCart] = useState([]);
-//      //const [cartView, setCartView] = useState(false)
-//      useEffect(() =>{
-//          fetch("http://localhost:3001/foods")
-//          .then(resp => resp.json())
-//          .then(foods => {
-//         const foodCart = foods.filter(food => !!food.cart)
-//         setCart(foodCart)
-//          })
-//      })
 
-//      const addToCart = cart.map(food => <FoodCard  key={food.id} food={food}/>)
+function Cart() {
+    const [cart, setCart] = useState([]);
+     
+     useEffect(() =>{
+         fetch("http://localhost:3001/foods")
+         .then(resp => resp.json())
+         .then(foods => {
+        const foodCart = foods.filter(food => !!food.cart)
+        setCart(foodCart)
+         })
+     })
+
+     const addToCart = cart.map(food => <FoodCard  key={food.id} food={food}/>)
     
-//     //  const addToCart = (food) => {
-//     //     if(!cart.find(atc => atc.name === food.name)) {
-//     //         setCart(currentCart => [...currentCart, food])
-//     //         alert (`${food.name} was  added to cart!`)
-//     //    }
-//     // }
-//     return (
-//         <div>
-//             {addToCart}
-//         </div>
-//     )
-// }
+    
+    return (
+        <div>
+            {addToCart}
+        </div>
+    )
+}
 
-// export default Cart
+export default Cart
